f5394b0ba233c9099d5fe0976ec93f6be33798f0
Brett Heinkel <bheinks@gmail.com>
2019-08-29 13:25:36 -0500

    Initial commit

---
+++ check.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Where to find sample input files
+INPUT="./input"
+
+# Where to place the actual output of running the program
+OUTPUT="./output"
+
+# Where to find the expected output
+EXPECTED="./expected"
+
+# Where to place generated diffs
+REPORTS="./reports"
+
+# The name of the executable file
+EXEC=./main.py
+
+green=`tput setaf 2`
+red=`tput setaf 1`
+reset=`tput sgr0`
+
+fails=0
+passes=0
+
+for f in $INPUT/*; do
+    filename=$(basename $f) # strip path
+    testname="${filename%.*}" # strip extension
+
+    # actually run file
+    ${EXEC} "$INPUT/$filename" > "$OUTPUT/$filename.out"
+
+    # run diff
+    diff -yib "$OUTPUT/$filename.out" "$EXPECTED/$filename.out" > "$REPORTS/$filename"
+
+    if [ $? -ne 0 ]; then
+        fails=$[ $fails + 1 ]
+        echo "check: ${red}[fail]${reset} $testname"
+        head "$REPORTS/$filename"
+    else
+        passes=$[ $passes + 1 ]
+        echo "check: ${pass}[pass]${reset} $testname"
+    fi
+done
+
+echo "check: ${green}$passes tests passed${reset}"
+echo "check: ${red}$fails tests failed${reset}"
---
+++ main.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+
+# Set True to disable console output, False otherwise
+SUPPRESS_OUTPUT = False
+
+# Reserved words
+KEYWORDS = {
+    'and':      'T_AND',
+    'array':    'T_ARRAY',
+    'begin':    'T_BEGIN',
+    'boolean':  'T_BOOL',
+    'char':     'T_CHAR',
+    'div':      'T_DIV',
+    'do':       'T_DO',
+    'else':     'T_ELSE',
+    'end':      'T_END',
+    'false':    'T_FALSE',
+    'if':       'T_IF',
+    'integer':  'T_INT',
+    'not':      'T_NOT',
+    'of':       'T_OF',
+    'or':       'T_OR',
+    'procedure':'T_PROC',
+    'program':  'T_PROG',
+    'read':     'T_READ',
+    'then':     'T_THEN',
+    'true':     'T_TRUE',
+    'var':      'T_VAR',
+    'while':    'T_WHILE',
+    'write':    'T_WRITE',
+}
+
+# Token pattern specification
+TOKEN_SPEC = (
+    ('T_INTCONST',    r'\d+'),          # Integer constants
+    ('T_CHARCONST',   r"'.'"),          # Character constants
+    ('T_IDENT',       r'[A-Za-z_]\w*'), # Identifiers
+    ('T_DOTDOT',      r'\.\.'),         # Begin operators
+    ('T_ASSIGN',      r':='),
+    ('T_LE',          r'<='),
+    ('T_NE',          r'<>'),
+    ('T_GE',          r'>='),
+    ('T_LPAREN',      r'\('),              
+    ('T_RPAREN',      r'\)'),
+    ('T_MULT',        r'\*'),
+    ('T_PLUS',        r'\+'),
+    ('T_COMMA',       r','),
+    ('T_MINUS',       r'-'),
+    ('T_DOT',         r'\.'),
+    ('T_COLON',       r':'),
+    ('T_SCOLON',      r';'),
+    ('T_LT',          r'<'),
+    ('T_EQ',          r'='),
+    ('T_GT',          r'>'),
+    ('T_LBRACK',      r'\['),
+    ('T_RBRACK',      r']'),            # End operators
+    ('INVALIDCHAR',   r"''"),           # Invalid character constant
+    ('WHITESPACE',    r'[ \t\r\n]+'),   # Whitespace
+    ('UNKNOWN',       r'.'),            # Everything else
+)
+
+
+def print_token(token, lexeme):
+    print(f'TOKEN: {token:<12}LEXEME: {lexeme}')
+
+
+def valid_integer(intconst):
+    # Python supports arbitrarily large integers, so there's no chance of overflow
+    return int(intconst) <= 2147483647
+
+
+def main(input_filename):
+    if SUPPRESS_OUTPUT:
+        sys.stdout = None
+
+    with open(input_filename) as f:
+        input_file = f.read()
+
+        # Strip out all comments
+        input_file = re.sub('\(\*.*?\*\)', '', input_file, flags=re.DOTALL)
+
+        # Build token regex
+        token_regex = '|'.join(f'(?P<{token}>{pattern})' for token, pattern in TOKEN_SPEC)
+
+        # Loop over every match
+        for match in re.finditer(token_regex, input_file, flags=re.ASCII):
+            token = match.lastgroup
+            lexeme = match.group()
+
+            # Integer constants
+            if token == 'T_INTCONST':
+                if not valid_integer(lexeme):
+                    print(f'**** Invalid integer constant: {lexeme}')
+                    continue
+            # Identifiers
+            elif token == 'T_IDENT' and lexeme in KEYWORDS:
+                token = KEYWORDS[lexeme]
+            # Whitespace
+            elif token == 'WHITESPACE':
+                continue
+            # Invalid character constants
+            elif token == 'INVALIDCHAR' or (token == 'UNKNOWN' and lexeme.startswith("'")):
+                print(f'**** Invalid character constant: {lexeme}')
+                continue
+
+            # Print token info
+            print_token(token, lexeme)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
