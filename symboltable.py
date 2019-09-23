795979de9ba606839d16a41ed89eb7d480bd9b1c
Brett Heinkel <bheinks@gmail.com>
2019-09-22 23:57:26 -0500

    Add SymbolTable class

---
+++ symboltable.py
@@ -0,0 +1,35 @@
+class SymbolTable:
+    def __init__(self):
+        # Represented as a dictionary
+        self.table = {}
+
+    # Add a new entry to the symbol table
+    def add(self, name, var_type, bounds, base_type):
+        print(f'\n+++ Adding {name} to symbol table with type {var_type}', end='')
+
+        # If array
+        if bounds and base_type:
+            print(' {} .. {} OF {}'.format(*bounds, base_type))
+        else:
+            print()
+            
+        # If symbol is already defined in table
+        if name in self.table:
+            return False
+
+        self.table[name] = Entry(name, var_type, bounds, base_type)
+
+        return True
+
+    # Override "in" operator
+    def __contains__(self, key):
+        return key in self.table
+
+
+# Symbol table entry
+class Entry:
+    def __init__(self, name, var_type, bounds, base_type):
+        self.name = name
+        self.var_type = var_type
+        self.bounds = bounds
+        self.base_type = base_type
